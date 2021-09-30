2a74849016ca46244965bfd46f203e6765b4ae86
Nick <nwharr12@gmail.com>
2021-09-30 14:26:47 -0500

    second commit

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-$(document).ready(function () {
-    var h3 = $("<h3></h3>");
-    var ul = $("input[type='text']")
-    $("body").append(h3)
+var friends = ['Garrett', 'Nick', 'Nathan', 'Brayden', 'Beth']
+var places = ['Home', 'Walmart', 'Chick-Fil-A', 'Grocery Store', 'Mall', 'McDonalds', 'Jacks', 'Home Depot', 'Papa Johns', 'Wendys']
+var weapons = ['Sword', 'Knife', 'Spear', 'Bow', 'Gun', 'AR-15', 'Pencil', 'Stick', 'Spoon', 'Paper', 'Chair', 'Table', 'Bat', 'Axe', 'Shotgun', 'Pen', 'Dagger', 'Staff', 'Brass knuckles', 'Broom'];
 
+for (var i = 1; i < 101; i++) {
+    var h3 = document.createElement("h3");
+    h3.textContent = "Accusation: " + i;
+    h3.addEventListener("click", captureIndex(i));
 
+    document.body.appendChild(h3);
+};
 
-var friends = ['Nick', 'garrett', 'Nathan']
-
-
-
-
-    h3.click(function () {
-        var text = $("Accusation i: I accuse , with the WEAPON_NAME in the LOCATION_NAME ex: Accusation 7: I accuse Jane, with the paper clip in the copy room!");
-var friends = append(h3)
-
-    });
-
-
-
-});
+function captureIndex(i) {
+    function name() {
+        alert("Accusation" + i + ": I accuse " + friends[i % friends.length] + ", with the " + weapons[i % weapons.length] + " in the " + places[i % places.length] + "!")
+    };
+    return name
+};
