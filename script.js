46e92f040547061b5fe6fbe4babaa92190e32b20
Nick <nwharr12@gmail.com>
2021-09-30 13:34:53 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var h3 = $("<h3></h3>");
+    var ul = $("input[type='text']")
+    $("body").append(h3)
+
+
+
+var friends = ['Nick', 'garrett', 'Nathan']
+
+
+
+
+    h3.click(function () {
+        var text = $("Accusation i: I accuse , with the WEAPON_NAME in the LOCATION_NAME ex: Accusation 7: I accuse Jane, with the paper clip in the copy room!");
+var friends = append(h3)
+
+    });
+
+
+
+});
